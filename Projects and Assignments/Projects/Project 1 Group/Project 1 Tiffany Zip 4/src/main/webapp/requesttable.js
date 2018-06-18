9b7ddee64f3df41c1bb626641a1e2b337346b108
Jacqueline Tummala <jacqtum@gmail.com>
2018-06-17 23:57:14 -0400

    Pega Notes

--- Projects and Assignments/Projects/Project 1 Group/Project 1 Tiffany Zip 4/src/main/webapp/requesttable.js
+++
@@ -1,6 +0,0 @@
-var result = document.getElementByID("cell");
-result.addEventListener("click", modal)
-function modal(){
-	var leftTable = document.getElementByID("lefttable");
-	console.log("here");
-}
